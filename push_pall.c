84e2eebaf50cf579b2bbd94e812803f6b03ff1f5
hajar <hajar.elrh@gmail.com>
2023-06-22 15:33:41 -0700

    UPDATED

--- push_pall.c
+++ push_pall.c
@@ -41,3 +41,21 @@ void f_push(stack_t **st, unsigned int c)
 	else
 		add_queue(st, a);
 }
+/**
+ * f_pall - print stack
+ * @st: head
+ * @c: no used
+ * Return: void
+ */
+void f_pall(stack_t **st, unsigned int c)
+{
+	stack_t *h;
+	(void)c;
+
+	h = *st;
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+}
